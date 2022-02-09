abd6726c7a6fdbdd44ed010d5200190c7f998550
Jose Villa <jgvillaperez@student.fullsail.educd>
2022-02-09 13:09:48 -0700

    began implementing grader for nodejs

---
+++ assignment1/nodeGrader.js
@@ -0,0 +1,62 @@
+// importing the readline library
+const { flip } = require("@popperjs/core")
+const readline = require("readline")
+
+class Grader {
+    constructor(name, assignment, grade){
+        this.name = name
+        this.assignment = assignment
+        this.grade = grade
+    }
+
+    GradeCalc() {
+        let grade = this.grade
+        if(grade >= 90 && grade <= 100){
+            return "Grade: A"
+        }
+        else if(grade >= 80 && grade <= 89){
+        return "Grade: B"
+        }
+        else if(grade >= 70 && grade <= 79){
+            return "Grade: C"
+        }
+        else if(grade >= 69 && grade <= 70){
+            return "Grade: D"
+        }
+        else {
+            return "Grade: F"
+        }        
+    }
+}
+
+function ValidateStringInput(message){
+    console.log(message)
+
+}
+
+// setting up the interface four the console
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout 
+})
+
+// getting inputs from the user
+let name = '',
+    assignment = '',
+    grade = ''
+
+rl.question("Enter the name of the student: ", (inputName) => {
+    name = inputNam
+    rl.question("Next, enter the assignment name: ", (inputAssignment) => {
+        assignment = inputAssignment
+        rl.question("Finally, enter the grade for " + assignment + ": ", (inputGrade) => {
+            grade = inputGrade
+            rl.close
+        })
+        rl.close
+    })
+    rl.close
+})
+
+let gradeAssignment = new Grader(name, assignment, grade)
+console.log("running")
