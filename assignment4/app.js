9c5c48bfd66e26b2dba29f4acc3b2861c6b4f956
Jose Villa <jgvillaperez@student.fullsail.educd>
2022-02-23 16:57:43 -0700

    removed unecessary variable

--- assignment4/app.js
+++ assignment4/app.js
@@ -1,18 +1,19 @@
 var http = require("http"); // 1. 'http' library misspelled
 
-var myname = () => { // 2. 'function' is misspelled
-  const message = "here is my IP adress";
-  return message; // 3. wasnt returning anything only console logging
+var myname = () => {
+  // 2. 'function' is misspelled
+  return "here is my IP adress"; // 3. wasnt returning anything only console logging
 };
 
-async function callHttpbin() { // 4. missing async keyword
+async function callHttpbin() {
+  // 4. missing async keyword
   // 5. 'callHttpbin' is misspelled does not match invokation
   let promise = new Promise((resolve, reject) => {
     http.get("http://httpbin.org/ip", (response) => {
       var str = "";
       response.setEncoding("utf8");
-      response.on("data", data => {
-        str += data; 
+      response.on("data", (data) => {
+        str += data;
       });
       response.on("end", () => {
         var result = JSON.parse(str);
@@ -26,7 +27,8 @@ async function callHttpbin() { // 4. missing async keyword
   return result; // 7. wasnt returning value
 }
 
-async function executeAsyncTask() { // 8. missing async keyword
+async function executeAsyncTask() {
+  // 8. missing async keyword
   const valueA = await callHttpbin();
   const valueB = myname();
   console.log(valueB + " " + valueA);
